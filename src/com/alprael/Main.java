3c86228d0695f0c217cc5b5d01bdb7ed2bacb3f7
Alex Rael <alprael22@gmail.com>
2019-07-23 23:51:35 -0600

    Initial commit

---
+++ src/com/alprael/Main.java
@@ -0,0 +1,16 @@
+package com.alprael;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        char mychar = 'D';
+        char myUniChar = '\u0044';
+        System.out.println(mychar);
+        System.out.println(myUniChar);
+
+        boolean myTrueBoolean = true;
+        boolean myFalseBoolean = false;
+        boolean isCustomerOverTwentyOne = true;
+    }
+}
